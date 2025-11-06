bf36331cf559b2cd55a7248f56043b7d9c4ea731
Andrew <andreytihonov447@gmail.com>
2025-11-06 15:33:42 +0300

    add token and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -15,7 +16,7 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Возвращает случайный токен
+  # Возвращает случайный токен.
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -25,6 +26,9 @@ class User < ActiveRecord::Base
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # Возвращает true, если предоставленный токен совпадает с дайджестом.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
-
-
